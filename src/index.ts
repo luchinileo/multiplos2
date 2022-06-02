2eb2e93b927451b24c9dca30e1f1ac5426d6e50b
luchinileo <104324324+luchinileo@users.noreply.github.com>
2022-06-01 21:08:03 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+rotulo1.innerHTML = "Indique el numero a verificar:";
+rotulo2.innerHTML = "Indique el numero multiplo:";
+
+let numero1: number = Number(dato1.value);
+let numero2: number = Number(dato2.value);
+
+function esMultiplo(numero1: number, numero2: number): boolean {
+  return numero1 % numero2 === 0;
+}
+
+if (esMultiplo(numero1, numero2)) {
+  console.log(numero1, "ES numero multiplo de ", numero2);
+} else {
+  console.log(numero1, "NO es numero multiplo de ", numero2);
+}
